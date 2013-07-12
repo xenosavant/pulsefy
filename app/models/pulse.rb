7f8e2b9a10c32366aeb207efd07711c60c663176
Domenick <Domenick.Packett@gmial.com>
2013-07-12 03:21:20 -0400

    1.4

--- app/models/pulse.rb
+++ app/models/pulse.rb
@@ -5,7 +5,7 @@ class Pulse < ActiveRecord::Base
   has_many :pulse_comments, :dependent => :destroy
   attr_accessible :depth, :content, :reinforcements, :degradations, :pulser_type, :pulser,
                   :tags, :link, :embed_code, :thumbnail, :link_type, :url, :headline
-  validates :content, :presence => true, :length => { :maximum => 250 }
+  validates :content, :presence => true, :length => { :maximum => 300 }
   validates :tags, :length => { :maximum => 50 }
   validates :headline, :length => { :maximum => 50 }
   validates :link, :allow_blank => true, :format => { :with => URI::regexp(%w(http https)), :message => "Valid URL required"}
