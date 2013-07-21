141e284c6bfae3c28a05ff5899a92db3c5f704c2
Domenick <Domenick.Packett@gmial.com>
2013-07-21 13:33:52 -0400

    1.0.5.5

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -1,11 +1,3 @@
-if !Rails.env.production?
-  require 'yaml'
-  config = YAML.load_file(Rails.root.join("config", "services.yml"))
-  ENV['AKIAJIHY77FC7AQOFPRA'] = config["s3"]["key"]
-  ENV['hYC8+RrSaZSFcjK/JeM9EOmlKvYJ5CWfk/3tRNMx'] = config["s3"]["secret"]
-  ENV['pulsefy'] = config["s3"]["bucket"]
-end
-
 CarrierWave.configure do |config|
   config.fog_credentials = {
       :provider               => 'AWS',
@@ -14,6 +6,5 @@ CarrierWave.configure do |config|
       :region => 'us-west-2'
   }
   config.asset_host = 's3-us-west-2.amazonaws.com'
-  config.fog_directory  = ENV['pulsefy']
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
+  config.fog_directory  = 'pulsefy'
 end
