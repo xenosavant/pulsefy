688fd7f1b25960d3767afed8f51422fcfaafd399
Domenick <Domenick.Packett@gmial.com>
2014-03-12 00:25:05 -0400

    2.0.0.3

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -20,11 +20,6 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  "#{Rails.root}/assets/images/Q300.jpg"
-  end
 
   def cache_dir
     "#{Rails.root}/tmp/uploads"
