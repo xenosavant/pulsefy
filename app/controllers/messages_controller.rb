fe839b1e19a5515a3fbaa95e7683eb581180dfcd
Domenick <Domenick.Packett@gmial.com>
2014-03-22 04:14:03 -0400

    2.0.6.6

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,7 +2,7 @@ class MessagesController < ApplicationController
   include SessionsHelper
 
   def create
-    if @id != current_node.id
+    if params[:receiver_id] != current_node.id
     @node = Node.find(params[:receiver_id])
     case !current_node.dialogues.where(:receiver_id => @node.id).exists? and !@node.dialogues.where(:receiver_id => current_node.id).exists?
       when true
