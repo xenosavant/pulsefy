998194743b616b84efd8dc37a014f4bf4eab8f8e
Domenick <Domenick.Packett@gmial.com>
2014-03-24 15:00:21 -0400

    2.0.9.4

--- app/controllers/dialogues_controller.rb
+++ app/controllers/dialogues_controller.rb
@@ -5,11 +5,11 @@ def new
 end
 
 def update
-  @dialogue = Message.find(params[:id])
+  @dialogue = Dialogue.find(params[:id])
 end
 
 def destroy
-  @dialogue = Message.find(params[:id])
+  @dialogue = Dialogue.find(params[:id])
   @dialogue.destroy
   redirect_to :controller => 'inboxes', :action => 'show_dialogues', :id => current_node.id
 end
