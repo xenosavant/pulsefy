1bb577d9e23ba7eb287748fed4a09d39abacd829
Domenick <Domenick.Packett@gmial.com>
2014-04-02 01:07:08 -0400

    2.1.7.0

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Pulsefy::Application.routes.draw do
   resources :dialogues
 
   root :to => 'static#home'
-
+  match '/reroute' => 'inboxes#route_mail'
   match '/message' => 'messages#new'
   match '/inbox' => 'inboxes#show_dialogues'
   match '/dialogue' => 'inboxes#show_convos'
