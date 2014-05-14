aa597a911adfc270bc645a318ff459bf7c15094f
Domenick <Domenick.Packett@gmial.com>
2014-05-14 15:11:59 -0400

    2.2.2.5

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,17 +5,17 @@ class MessagesController < ApplicationController
   def create
     if session[:receiver] != current_node.id
     @node = Node.find(session[:receiver])
-    case current_node.dialogues.where(:receiver_id => @node.id).exists? or current_node.dialogues.where(:sender_id => @node.id).exists?
-      when false
+    case current_node.dialogues.where(:receiver_id => @node.id).nil? or current_node.dialogues.where(:sender_id => @node.id).nil?
+      when true
         @dialogue = current_node.dialogues.build
         @dialogue.update_attributes(:sender_id => current_node.id, :receiver_id => @node.id, :unread_receiver => true, :unread_sender => false)
         @node.dialogues << @dialogue
         current_node << @dialogue
-      when true
-        if !current_node.dialogues.where(:receiver_id => @node.id).nil?
+      when false
+        if !current_node.dialogues.where(:receiver_id => @node.id).first.nil?
             @dialogue = current_node.dialogues.where(:receiver_id => @node.id).first
             @dialogue.update_attributes(:unread_receiver => true)
-          else if !current_node.dialogues.where(:sender_id => @node.id).nil?
+          else if !current_node.dialogues.where(:sender_id => @node.id).first.nil?
             @dialogue = current_node.dialogues.where(:sender_id => @node.id).first
             @dialogue.update_attributes(:unread_sender => true)
           end
