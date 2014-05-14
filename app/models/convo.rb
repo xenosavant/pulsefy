27e3384fd2f0dbaed97e206e403885b3cb375f02
Domenick <Domenick.Packett@gmial.com>
2014-05-13 22:31:33 -0400

    2.2.0.9

--- app/models/convo.rb
+++ app/models/convo.rb
@@ -3,7 +3,7 @@ class Convo < ActiveRecord::Base
   belongs_to :dialogue
   has_many :messages, :dependent => :destroy
   attr_accessible :interrogator_id, :interlocutor_id, :active, :unread_interrogator,
-                  :unread_receiver
+                  :unread_interlocutor
   default_scope order 'convos.created_at DESC'
 
 end
--- db/migrate/20140225194245_create_convos.rb
+++ db/migrate/20140225194245_create_convos.rb
@@ -4,7 +4,7 @@ class CreateConvos < ActiveRecord::Migration
       create_table :convos do |t|
         t.references :dialogue
         t.integer :interrogator_id, :interlocutor_id
-        t.boolean :active, :unread_interrogator, :unread_receiver
+        t.boolean :active, :unread_interrogator, :unread_interlocutor
         t.timestamps
       end
       add_index :convos, :dialogue_id
