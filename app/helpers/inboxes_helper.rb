478a94463ab20e587a6cd5eb019fc48b0bdcb0d5
Domenick <Domenick.Packett@gmial.com>
2014-05-22 03:39:22 -0400

    2.2.3.3

--- app/helpers/inboxes_helper.rb
+++ app/helpers/inboxes_helper.rb
@@ -17,7 +17,7 @@ module InboxesHelper
         end
       end
      end
-    @node.update_attribute(:unreads => tmp)
+    @node.update_attribute(:unreads, tmp)
   end
 
   def update_dialogues(args)
