de308f75fa61ada07dc5c72452407ccd03ef531a
Domenick <Domenick.Packett@gmial.com>
2014-07-06 14:25:14 -0400

    2.2.7.2

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,2 +1,3 @@
-uri = URI.parse(ENV['redis://redistogo:e386ea66cddba54d88d815e28b8b5ee9@grouper.redistogo.com:10058/'] || "redis://localhost:6379/" )
+uri = URI.parse(ENV['redis://redistogo:e386ea66cddba54d88d815e28b8b5ee9@grouper.redistogo.com:10058/'] )
 REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+Resque.redis = REDIS
