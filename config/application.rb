cff633cbf0864a707a558739df834beeb3dad876
Domenick <Domenick.Packett@gmial.com>
2014-07-07 21:33:29 -0400

    2.2.9.1

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require File.expand_path('../boot', __FILE__)
 require 'sprockets/railtie'
 require 'rails/all'
+require 'resque/server'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
