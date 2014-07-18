76c597aee9669298b631d6a70bc3c779fdfb4fee
Domenick <Domenick.Packett@gmial.com>
2014-07-18 02:04:12 -0400

    3.0.7.5

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,6 +4,6 @@ class Message < ActiveRecord::Base
   attr_accessible :content, :sender_id, :receiver_id
   validates :content, :presence => true, :length => { :maximum => 2500 }
 
-  default_scope order 'messages.created_at DSC'
+  default_scope order 'messages.created_at DESC'
 
 end
