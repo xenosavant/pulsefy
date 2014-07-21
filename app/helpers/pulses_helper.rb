d548475a338b554e47de1cecc417bc91225a4ce6
Domenick <Domenick.Packett@gmial.com>
2014-07-21 18:24:16 -0400

    3.0.8.3

--- app/helpers/pulses_helper.rb
+++ app/helpers/pulses_helper.rb
@@ -1,8 +1,8 @@
 module PulsesHelper
 
-  def return_pulser(pulse_id)
+  def current_pulser(pulse_id)
    temp_pulse = Pulse.find(pulse_id)
-   @pulser = Node.find(temp_pulse.pulser)
+   @current_pulser = Node.find(temp_pulse.pulser)
   end
 
 end
