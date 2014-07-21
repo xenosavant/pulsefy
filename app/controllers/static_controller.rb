8f3420e710cc2ae2f5271c46d81da94d48dcdfcd
Domenick <Domenick.Packett@gmial.com>
2014-07-20 23:59:20 -0400

    3.0.7.6

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,12 +1,13 @@
 class StaticController < ApplicationController
   include SessionsHelper
 
-def home
-  if signed_in?
-  @me = current_node
-  @pulse = current_node.pulses.new
-  store_location(0, 'Static')
-  @pulses = @node.pulses.paginate(:page => params[:page])
-  end
-end
+ def home
+   if signed_in?
+     @me = current_node
+     @pulse = current_node.pulses.new
+     store_location(0, 'Static')
+     @pulses = @me.pulses.paginate(:page => params[:page])
+   end
+ end
+
 end
