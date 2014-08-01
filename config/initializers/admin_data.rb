fa0f6cb73847c2e884d69b2420d12648bc08745e
Domenick <Domenick.Packett@gmial.com>
2014-08-01 00:16:08 -0400

    3.1.5.5

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,3 +1,6 @@
+include ApplicationController
+include SessionsHelper
+
 AdminData.config do |config|
   config.is_allowed_to_view = lambda {|controller| return true if current_node.is_an_admin? }
   config.is_allowed_to_update = lambda {|controller| return true if current_node.is_an_admin? }
