b0ef2ba84a0297c1290b6bca74ef756384ecfb73
Domenick <Domenick.Packett@gmial.com>
2014-08-06 00:27:42 -0400

    3.1.6.0

--- app/models/node.rb
+++ app/models/node.rb
@@ -15,6 +15,7 @@ class Node < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, :presence => true, :format => { :with => VALID_EMAIL_REGEX },
             :uniqueness => { :case_sensitive => false }, :on => :create
+  validates :self_tag, :presence => false, :uniqueness => { :case_sensitive => false }
   validates :password, :presence => true, :length => { :minimum => 6 }, :on => :create
   validates :password_confirmation, :presence => true, :on => :create
   validates :info, :length => { :maximum => 2000 }
