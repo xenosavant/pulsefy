f87d62bf46f07367e99c8dd50742d0a606ba7d25
Domenick <Domenick.Packett@gmial.com>
2014-12-19 03:49:39 -0500

    fixed messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,14 +3,11 @@ class MessagesController < ApplicationController
   include InboxesHelper
 
   def create
-    @message = Message.new(params[:message])
-    @message.init(current_node, session[:receiver]).messages.build(params[:message])
+    @convo = init(current_node, session[:receiver])
+    @unread = Node.find(session[:receiver]).unreads.build
+    @unread.update_attributes(:convo_id => @convo.id)
+    @message = @convo.messages.build(params[:message])
     @message.update_attributes(:read => false, :receiver_id => session[:receiver], :sender_id => current_node.id)
-    @message.save
-        @unread = @node.unreads.build
-        @unread.update_attributes(:convo_id => @convo.id)
-
-    @message.init(current_node, session[:receiver])
     if @message.save
       redirect_to :controller => 'inboxes', :action => 'show_messages',
                  :id => @convo.id, :errors => @message.errors.full_messages
