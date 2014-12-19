72c787e2b1470a52701610b82ddb639777fad204
Domenick <Domenick.Packett@gmial.com>
2014-12-19 04:33:01 -0500

    Fixed blank Message Issue

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,18 +3,23 @@ class MessagesController < ApplicationController
   include InboxesHelper
 
   def create
+    case params[:message].content
+      when true
     @convo = current_node.initialize_message(session[:receiver])
     @unread = Node.find(session[:receiver]).unreads.build
     @unread.update_attributes(:convo_id => @convo.id)
     @message = @convo.messages.build(params[:message])
     @message.update_attributes(:read => false, :receiver_id => session[:receiver], :sender_id => current_node.id)
-    if @message.save
-      redirect_to :controller => 'inboxes', :action => 'show_messages',
+      if @message.save
+        redirect_to :controller => 'inboxes', :action => 'show_messages',
                  :id => @convo.id, :errors => @message.errors.full_messages
-    else
-      redirect_to :controller => 'messages', :action => 'new',
+      else
+        redirect_to :controller => 'messages', :action => 'new',
                      :id => @node.id, :errors => @message.errors.full_messages
-    end
+      end
+     else
+      redirect_to :controller => 'messages', :action => 'new',
+                    :id => @node.id, :errors => @message.errors.full_messages
   end
 
   def new
