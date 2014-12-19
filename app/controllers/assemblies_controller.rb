68d6d13c6890afb6ba8ebd4104c866d33717cffd
Domenick <Domenick.Packett@gmial.com>
2014-12-19 04:18:30 -0500

    Fixed Assemblies

--- app/controllers/assemblies_controller.rb
+++ app/controllers/assemblies_controller.rb
@@ -15,7 +15,7 @@ class AssembliesController < ApplicationController
          render 'crop'
        end
     else
-      render 'new'
+      redirect_to :controller => 'assemblies', :action => 'new', :errors => @assembly.errors.full_messages
     end
   end
 
