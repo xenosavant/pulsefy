ea206298c9defffc810764b1578e0277f47692c1
Domenick <Domenick.Packett@gmial.com>
2014-12-19 03:16:37 -0500

    messages fixed

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(params[:message])
-    @message.init
+    @message.init(current_node)
     if @message.save
       redirect_to :controller => 'inboxes', :action => 'show_messages',
                  :id => @convo.id, :errors => @message.errors.full_messages
--- app/models/message.rb
+++ app/models/message.rb
@@ -10,28 +10,28 @@ class Message < ActiveRecord::Base
     @current_sender = Node.find(self.sender_id)
   end
 
-  def init
-    if session[:receiver] !=  current_node.id and session[:receiver] != 0 and !session[:receiver].nil?
+  def init(node)
+    if session[:receiver] !=  node.id and session[:receiver] != 0 and !session[:receiver].nil?
       @node = Node.find(session[:receiver])
-      case current_node.dialogues.where(:receiver_id => @node.id).exists? or current_node.dialogues.where(:sender_id => @node.id).exists?
+      case node.dialogues.where(:receiver_id => @node.id).exists? or node.dialogues.where(:sender_id => @node.id).exists?
         when false
-          @dialogue = current_node.dialogues.build
-          @dialogue.update_attributes(:sender_id => current_node.id, :receiver_id => @node.id, :unread_receiver => true, :unread_sender => false)
+          @dialogue = node.dialogues.build
+          @dialogue.update_attributes(:sender_id => node.id, :receiver_id => @node.id, :unread_receiver => true, :unread_sender => false)
           @node.dialogues << @dialogue
-          current_node.dialogues << @dialogue
+          node.dialogues << @dialogue
         when true
-          case current_node.dialogues.where(:receiver_id => @node.id).exists?
+          case node.dialogues.where(:receiver_id => @node.id).exists?
             when true
-              @dialogue = current_node.dialogues.where(:receiver_id => @node.id).first
+              @dialogue = node.dialogues.where(:receiver_id => @node.id).first
               @dialogue.update_attributes(:unread_receiver => true)
             when false
-              @dialogue = current_node.dialogues.where(:sender_id => @node.id).first
+              @dialogue = node.dialogues.where(:sender_id => @node.id).first
               @dialogue.update_attributes(:unread_sender => true)
             else
-              @dialogue = current_node.dialogues.build
-              @dialogue.update_attributes(:sender_id => current_node.id, :receiver_id => @node.id, :unread_receiver => true, :unread_sender => false)
+              @dialogue = node.dialogues.build
+              @dialogue.update_attributes(:sender_id => node.id, :receiver_id => @node.id, :unread_receiver => true, :unread_sender => false)
               @node.dialogues << @dialogue
-              current_node.dialogues << @dialogue
+              node.dialogues << @dialogue
           end
       end
       @dialogue.save
@@ -46,12 +46,12 @@ class Message < ActiveRecord::Base
                     @old_convo.update_attributes(:active => false)
                     @old_convo.save
                     @convo = @dialogue.convos.build
-                    @convo.update_attributes(:interlocutor_id => @node.id, :interrogator_id => current_node.id,
+                    @convo.update_attributes(:interlocutor_id => @node.id, :interrogator_id => node.id,
                                              :unread_interrogator => false, :unread_interlocutor => true, :active => true)
                     @convo.save
                   else
                     @convo = @dialogue.convos.where(:active => true).last
-                    if @convo.interlocutor_id == current_node.id
+                    if @convo.interlocutor_id == node.id
                       @convo.update_attributes(:unread_interrogator => true)
                     else
                       @convo.update_attributes(:unread_interlocutor => true)
@@ -59,7 +59,7 @@ class Message < ActiveRecord::Base
                   end
                 else
                   @convo = @dialogue.convos.where(:active => true).last
-                  if @convo.interlocutor_id == current_node.id
+                  if @convo.interlocutor_id == node.id
                     @convo.update_attributes(:unread_interrogator => true)
                   else
                     @convo.update_attributes(:unread_interlocutor => true)
@@ -67,18 +67,18 @@ class Message < ActiveRecord::Base
               end
             else
               @convo = @dialogue.convos.build
-              @convo.update_attributes(:interlocutor_id => @node.id, :interrogator_id => current_node.id,
+              @convo.update_attributes(:interlocutor_id => @node.id, :interrogator_id => node.id,
                                        :unread_interrogator => false, :unread_interlocutor => true, :active => true)
               @convo.save
           end
         else
           @convo = @dialogue.convos.build
-          @convo.update_attributes(:interlocutor_id => @node.id, :interrogator_id => current_node.id,
+          @convo.update_attributes(:interlocutor_id => @node.id, :interrogator_id => node.id,
                                    :unread_interrogator => false, :unread_interlocutor => true, :active => true)
           @convo.save
       end
       @message = @convo.messages.build(params[:message])
-      @message.update_attributes(:read => false, :receiver_id => @node.id, :sender_id => current_node.id)
+      @message.update_attributes(:read => false, :receiver_id => @node.id, :sender_id => node.id)
       @message.save
       case @node.unreads.where("convo_id = ?", @convo.id).first.nil?
         when true
